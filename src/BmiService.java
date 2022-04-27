c7429c9b6f871ed0bf3c3108b4ceff0eef0abf90
Марьяна Варфоломеева <mihralievam@icloud.com>
2022-04-27 12:36:10 +0500

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double weight, double height){
-        double height_mtr=height/100;
-        return weight/height_mtr/height_mtr;
+    public double calculate(double weight, double height) {
+        double height_mtr = height / 100;
+        return weight / height_mtr / height_mtr;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        double m_kg=77; // масса в кг
-        double h_cm= 170;// рост в см
-        BmiService bmiService=new BmiService();
-        double bmi=bmiService.calculate(m_kg,h_cm);
+        double m_kg = 77; // масса в кг
+        double h_cm = 170;// рост в см
+        BmiService bmiService = new BmiService();
+        double bmi = bmiService.calculate(m_kg, h_cm);
         System.out.println(bmi);
-
     }
 
 }
