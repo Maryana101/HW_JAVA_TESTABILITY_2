1d92b8367f6af84f2c9b0f1c11373fb3ace6a4a6
Марьяна Варфоломеева <mihralievam@icloud.com>
2022-04-28 16:27:44 +0500

    Переменные исправлены согласно стилю camelСase

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        double height_mtr = height / 100;
-        return weight / height_mtr / height_mtr;
+        double heightMtr = height / 100;
+        return weight / heightMtr / heightMtr;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        double m_kg = 77; // масса в кг
-        double h_cm = 170;// рост в см
+        double mKg = 77; // масса в кг
+        double hCm = 170;// рост в см
         BmiService bmiService = new BmiService();
-        double bmi = bmiService.calculate(m_kg, h_cm);
+        int bmi = (int) bmiService.calculate(mKg, hCm);
         System.out.println(bmi);
     }
-
 }
